cb2b07ae634f24ad68d97e8caf6eeade5590042e
spacemanspiff2007 <accounts@sgerber.de>
2018-09-26 17:32:58 +0200

    Release 0.1.0

--- setup.py
+++ setup.py
@@ -14,11 +14,12 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/spacemanspiff2007/PyArtNet",
     packages=setuptools.find_packages(exclude=['tests']),
+    python_requires='>=3.6',
     classifiers=[
         "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3 :: Only"
+        "Programming Language :: Python :: 3 :: Only",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
     ],
