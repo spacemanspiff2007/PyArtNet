d182186298e6aa7965b6db56e2cdb5dc37e6b313
spacemanspiff2007 <10754716+spacemanspiff2007@users.noreply.github.com>
2025-10-01 09:53:15 +0200

    try uv

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,6 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
 
 
 # -- Options for exec code -------------------------------------------------
